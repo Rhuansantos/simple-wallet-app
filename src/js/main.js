204d304bb6039eed1e2302b076aa019dc242052b
Rhuan Santos <rhuansantosdev@gmail.com>
2017-11-04 23:11:06 -0400

    fixing flexbox and adjusting media queries

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',() => {
+
+  const left = document.getElementsByClassName('left');
+
+  
+});
+
+document.getElementsByClassName('left').addEventListener("click", function(){ alert("Hello World!"); });
