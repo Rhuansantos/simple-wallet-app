ad27c497fe43d58416dfde0a55e5db0bd1bebae0
Rhuan Santos <rhuansantosdev@gmail.com>
2017-11-26 16:52:56 -0500

    deleting model calls

--- src/app.js
+++ src/app.js
@@ -4,19 +4,14 @@ const bodyParser = require('body-parser');
 const pug = require('pug');
 
 module.exports = function(app) {
-  
+
   // Configure view engine to render PUG templates
   app.set('views', __dirname + '/views');
   app.set('view engine', 'pug');
   
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
-
-  let models = glob.sync(__dirname + '/models/*.js');
-  models.forEach(function(modelFileName){
-    require(modelFileName);
-  });
-
+  
   // include all the controllers
   let controllers = glob.sync(__dirname + '/controllers/*.js');
   controllers.forEach(function(controllerFileName){
